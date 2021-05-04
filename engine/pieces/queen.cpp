9d8bd07fd00fa354c7b1b636a0fe64ea6ed27ae1
Tobias Hettler <Tobias-Hettler@protonmail.com>
2021-05-04 17:24:35 +0200

    removed comment

--- engine/pieces/queen.cpp
+++ engine/pieces/queen.cpp
@@ -14,10 +14,10 @@ std::vector<Move> Queen::getMoves(U64 queens, U64 ownPieces, U64 enemyPieces) {
     std::vector<Move> movesSouthWest = getSlidingMovesSouthWest(queens, ownPieces, enemyPieces);
     std::vector<Move> movesNorthWest = getSlidingMovesNorthWest(queens, ownPieces, enemyPieces);
 
-    /*moves.insert(moves.end(), std::begin(movesNorth), std::end(movesNorth));
+    moves.insert(moves.end(), std::begin(movesNorth), std::end(movesNorth));
     moves.insert(moves.end(), std::begin(movesEast), std::end(movesEast));
     moves.insert(moves.end(), std::begin(movesSouth), std::end(movesSouth));
-    moves.insert(moves.end(), std::begin(movesWest), std::end(movesWest));*/
+    moves.insert(moves.end(), std::begin(movesWest), std::end(movesWest));
 
     moves.insert(moves.end(), std::begin(movesNorthEast), std::end(movesNorthEast));
     moves.insert(moves.end(), std::begin(movesSouthEast), std::end(movesSouthEast));
